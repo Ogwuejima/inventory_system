357274bb6312858408b912aa16efd9a3f19f8b41
Suleiman Hassan <ogwuejima@gmail.comgit config --global user.name Suleiman>
2025-07-24 10:31:32 +0100

    Prepare for Render deployment

--- inventory_system/settings.py
+++ inventory_system/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-m&b)w#-*ao9w1j58*i9eodysi5idjj9dfry5t(0(!avx5d*tyy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['inventory_system.onrender.com']
 
 
 # Application definition
@@ -159,3 +160,5 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
